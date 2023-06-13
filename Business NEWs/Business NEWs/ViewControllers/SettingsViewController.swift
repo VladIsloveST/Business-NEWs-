18f3a266341db450dabfacd723f59f1864c496e9
VladIsloveST <99729302+VladIsloveST@users.noreply.github.com>
2023-06-13 21:12:08 +0300

    Add TabBar programatically

---
+++ Business NEWs/Business NEWs/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Business NEWs
+//
+//  Created by Mac on 13.06.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
---
+++ Business NEWs/Business NEWs/ViewControllers/TabBar.swift
@@ -0,0 +1,46 @@
+//
+//  TabBar.swift
+//  Business NEWs
+//
+//  Created by Mac on 13.06.2023.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.tintColor = .label
+        setupVCs()
+    }
+    
+    func setupVCs() {
+            viewControllers = [
+                createNavController(for: ModuleBuilder.createSearchBuilder(),
+                                    title: NSLocalizedString("Saved", comment: ""),
+                                    imageName: "bookmark"),
+                createNavController(for: HomeViewController(),
+                                    title: NSLocalizedString("Home", comment: ""),
+                                    imageName: "house"),
+                createNavController(for: SettingsViewController(),
+                                    title: NSLocalizedString("Settings", comment: ""),
+                                    imageName: "gearshape")
+            ]
+    }
+    
+    fileprivate func createNavController(for rootViewController: UIViewController,
+                                         title: String,
+                                         imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        let image = UIImage(systemName: imageName)
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+}
